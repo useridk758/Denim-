dd3176d429b2212737a49adf45fe8cf31768b4fa
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-03-21 15:41:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const input = document.querySelector('input');
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
-    
-    // Register the proxy's "engine"
-    await window.navigator.serviceWorker.register('./uv/uv.sw.js', {
-        scope: __uv$config.prefix
-    });
 
-    let url = input.value.trim();
-    if (!url.includes('.')) url = 'https://www.google.com' + url;
-    else if (!/^http(s?):\/\//.test(url)) url = 'https://' + url;
+    // Register UV Service Worker
+    try {
+        await window.navigator.serviceWorker.register('./uv/uv.sw.js', {
+            scope: __uv$config.prefix
+        });
 
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        let url = input.value.trim();
+        if (!url.includes('.')) url = 'https://www.google.com' + url;
+        else if (!/^http(s?):\/\//.test(url)) url = 'https://' + url;
+
+        // This line opens the URL within your proxy context
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    } catch (err) {
+        alert("Proxy failed to load. Make sure you uploaded the UV folder!");
+        console.error(err);
+    }
 });
